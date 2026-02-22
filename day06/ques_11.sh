450073baccbc70e0ac5fca6a36cbe099da029ad1
Aditya Panchal <adityapanchal816@gmail.com>
2026-02-22 16:25:12 +0530

    Modified script

--- day06/ques_11.sh
+++ day06/ques_11.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 check_file(){
-read -p "Write the name of the file: $1"
+read -p "Write the name of the file:" filename
 
 #Check file exists or not
-if [ -e $1 ]
+if [ -e $filename ]
 then 
 	echo "File exists"
-	echo "Size of the file: $(ls -lh "$1" | awk '{print $5}')"
+	echo "Size of the file: $(ls -lh "$filename" | awk '{print $5}')"
 else 
 	echo "File does not exists"
 fi
